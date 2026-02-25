81c2590edd19ccb37c1ba5e0ab3f2ec8de24fea3
Cjsarts0509 <4rumarts@gmail.com>
2026-02-26 02:50:19 +0900

    chore: src 폴더 초기화를 위해 삭제

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
-import "./styles/index.css"; 
-import "./styles/tailwind.css"; 
-import "./styles/theme.css"; 
-import "./styles/fonts.css"; 
-createRoot(document.getElementById("root")!).render(<App />);
